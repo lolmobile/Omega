120f7e65d0a46034386d59a944ef601cb8b96ea7
lolmobile <68462239+lolmobile@users.noreply.github.com>
2020-07-18 17:19:30 +0800

    Update app.js

--- app.js
+++ app.js
@@ -3,17 +3,16 @@
 
 ///Edit Under This
 
-var firebaseConfig = {
-  apiKey: "api-key",
-  authDomain: "project-id.firebaseapp.com",
-  databaseURL: "https://project-id.firebaseio.com",
-  projectId: "project-id",
-  storageBucket: "project-id.appspot.com",
-  messagingSenderId: "sender-id",
-  appId: "app-id",
-  measurementId: "G-measurement-id",
+const firebaseConfig = {
+  apiKey: "AIzaSyCZ_7vnRYLqQq_Zxp9okn6uEV_UXuT6ruQ",
+  authDomain: "facebook-b1d94.firebaseapp.com",
+  databaseURL: "https://facebook-b1d94.firebaseio.com",
+  projectId: "facebook-b1d94",
+  storageBucket: "facebook-b1d94.appspot.com",
+  messagingSenderId: "1087872291323",
+  appId: "1:1087872291323:web:34cd45e0be03c131573b5e",
+  measurementId: "G-WKG95WV0EV"
 };
-
      
  
 ////Do Not Edit Below This
